75cdc83267c25c856732523fcaacc6eee284e023
KUBO, Yoshihito <kubo@jy-info-sys.com>
2022-06-21 20:26:02 +0900

    Fractionクラスとテストコード

---
+++ src/Fraction.java
@@ -0,0 +1,382 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * 分数クラス.
+ * @author 久保　由仁
+ */
+public class Fraction extends Number implements Serializable, Comparable<Fraction>
+{
+    /** 分子. */
+    protected BigInteger numerator = BigInteger.ZERO;
+    /** 分母. */
+    protected BigInteger denominator = BigInteger.ONE;
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 文字列からFractionを作成する.
+     * @param val 解析対象の文字列
+     * @return 作成したFraction
+     */
+    public static Fraction parse(final String val)
+    {
+        if(val == null) return null;
+        Pattern pattern = Pattern.compile("^([\\-\\+])?\\s*(\\d+)\\s*/\\s*(\\d+)$");
+        Matcher matcher = pattern.matcher(val.trim());
+        if(matcher.find())
+        {
+            BigInteger numeratorValue = new BigInteger(matcher.group(2));
+            BigInteger denominatorValue = new BigInteger(matcher.group(3));
+            Fraction frac = new Fraction(numeratorValue, denominatorValue);
+            if(matcher.group(1) != null && matcher.group(1).equals("-"))
+            {
+                frac = frac.multiply(-1);
+            }
+            return frac;
+        }
+        else
+        {
+            throw new IllegalArgumentException(val);
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタ.
+     * @param numerator 分子
+     * @param denominator 分母
+     */
+    public Fraction(final BigInteger numerator, final BigInteger denominator)
+    {
+        if(denominator == null || numerator == null) throw new NullPointerException();
+        if(denominator.equals(BigInteger.ZERO)) throw new IllegalArgumentException();
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタ（整数を1を分母とする分数に変換する）.
+     * @param numerator 整数
+     */
+    public Fraction(final BigInteger numerator)
+    {
+        this(numerator, BigInteger.ONE);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタ.
+     * @param numerator 分子
+     * @param denominator 分母
+     */
+    public Fraction(final long numerator, final long denominator)
+    {
+        this(new BigInteger(Long.toString(numerator)), new BigInteger(Long.toString(denominator)));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタ（整数を1を分母とする分数に変換する）.
+     * @param numerator 整数
+     */
+    public Fraction(final long numerator)
+    {
+        this(new BigInteger(Long.toString(numerator)));
+    }
+    //----------------------------------------------------------------------------------------------
+    /** デフォルトコンストラクタ. */
+    protected Fraction() {}
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 加算を行う
+     * @param another 加算対象の分数
+     * @return 加算結果
+     */
+    public Fraction add(final Fraction another)
+    {
+        if(another == null) throw new NullPointerException();
+        BigInteger gcd = denominator.gcd(another.denominator);
+        BigInteger lcm = denominator.multiply(another.denominator).divide(gcd);
+        BigInteger num1 = lcm.divide(denominator).multiply(numerator);
+        BigInteger num2 = lcm.divide(another.denominator).multiply(another.numerator);
+        Fraction result = new Fraction();
+        result.denominator  = lcm;
+        result.numerator = num1.add(num2);
+        return result.reduce();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 加算を行う
+     * @param another 加算対象の整数
+     * @return 加算結果
+     */
+    public Fraction add(final BigInteger another)
+    {
+        return add(new Fraction(another));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 加算を行う
+     * @param another 加算対象の整数
+     * @return 加算結果
+     */
+    public Fraction add(final long another)
+    {
+        return add(new Fraction(another));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 減算を行う.
+     * @param another 減算対象の分数
+     * @return 減算結果
+     */
+    public Fraction subtract(final Fraction another)
+    {
+        if(another == null) throw new NullPointerException();
+        return add(another.multiply(-1));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 減算を行う.
+     * @param another 減算対象の整数
+     * @return 減算結果
+     */
+    public Fraction subtract(final BigInteger another)
+    {
+        return subtract(new Fraction(another));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 減算を行う.
+     * @param another 減算対象の整数
+     * @return 減算結果
+     */
+    public Fraction subtract(final long another)
+    {
+        return subtract(new Fraction(another));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 乗算を行う.
+     * @param another 乗算対象の分数
+     * @return 乗算結果
+     */
+    public Fraction multiply(final Fraction another)
+    {
+        if(another == null) throw new NullPointerException();
+        Fraction result = new Fraction();
+        result.numerator = numerator.multiply(another.numerator);
+        result.denominator = denominator.multiply(another.denominator);
+        return result.reduce();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 乗算を行う.
+     * @param another 乗算対象の整数
+     * @return 乗算結果
+     */
+    public Fraction multiply(final BigInteger another)
+    {
+        if(another == null) throw new NullPointerException();
+        Fraction result = new Fraction();
+        result.numerator = numerator.multiply(another);
+        result.denominator = denominator;
+        return result.reduce();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 乗算を行う.
+     * @param another 乗算対象の整数
+     * @return 乗算結果
+     */
+    public Fraction multiply(final long another)
+    {
+        return multiply(new BigInteger(Long.toString(another)));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 除算を行う.
+     * @param another 除算対象の分数
+     * @return 除算結果
+     */
+    public Fraction divide(final Fraction another)
+    {
+        if(another == null) throw new NullPointerException();
+        if(another.numerator.equals(BigInteger.ZERO)) throw new IllegalArgumentException();
+        Fraction result = new Fraction();
+        result.numerator = numerator.multiply(another.denominator);
+        result.denominator = denominator.multiply(another.numerator);
+        return result.reduce();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 除算を行う.
+     * @param another 除算対象の整数
+     * @return 除算結果
+     */
+    public Fraction divide(final BigInteger another)
+    {
+        if(another == null) throw new NullPointerException();
+        if(another.equals(BigInteger.ZERO)) throw new IllegalArgumentException();
+        Fraction result = new Fraction();
+        result.numerator = numerator;
+        result.denominator = denominator.multiply(another);
+        return result.reduce();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 除算を行う.
+     * @param another 除算対象の整数
+     * @return 除算結果
+     */
+    public Fraction divide(final long another)
+    {
+        return divide(new BigInteger(Long.toString(another)));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 値が負か否かを判定する.
+     * @return true: 負, false: 正
+     */
+    public boolean isNegative()
+    {
+        BigInteger zero = BigInteger.ZERO;
+        if(numerator.equals(zero))
+        {
+            return false;
+        }
+        else
+        {
+            return (numerator.compareTo(zero) >= 0) ^ (denominator.compareTo(zero) >= 0);
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 約分する.
+     * @return 約分した分数
+     */
+    protected Fraction reduce()
+    {
+        BigInteger gcd = numerator.gcd(denominator);
+        return new Fraction(numerator.divide(gcd), denominator.divide(gcd));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の分数との大小を比較する.
+     * @param another 比較対象の分数
+     * @return 大小比較の結果
+     */
+    @Override
+    public int compareTo(final Fraction another)
+    {
+        if(another == null)
+        {
+            throw new NullPointerException();
+        }
+        Fraction diff = subtract(another);
+        if(diff.numerator.equals(BigInteger.ZERO))
+        {
+            return 0;
+        }
+        return diff.isNegative() ? -1 : 1;
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 分数の文字列表現を返す.
+     * @return この分数を表す文字列
+     */
+    @Override
+    public String toString()
+    {
+        if(denominator == null || numerator == null) return "";
+        String sign = isNegative() ? "-" : "";
+        Fraction reduced = this.reduce();
+        BigInteger newNumerator = reduced.numerator.abs();
+        BigInteger newDenominator = reduced.denominator.abs();
+        if(newDenominator.equals(BigInteger.ONE))
+        {
+            return String.format("%s%d", sign, newNumerator);
+        }
+        else
+        {
+            return String.format("%s (%d / %d)", sign, newNumerator, newDenominator).trim();
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 値が等しいか判定する.
+     * @param another 比較対象オブジェクト
+     * @return 等値の場合：true
+     */
+    @Override
+    public boolean equals(final Object another)
+    {
+        if(another == this) return true;
+        if(another == null) return false;
+        if(another instanceof Fraction)
+        {
+            return compareTo((Fraction)another) == 0;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * このオブジェクトのhashCodeを返す.
+     * @return hashCode
+     */
+    @Override
+    public int hashCode()
+    {
+        Fraction reduced = reduce();
+        int hash = 7;
+        hash = 31 * hash + reduced.numerator.intValue();
+        hash = 31 * hash + reduced.denominator.intValue();
+        return hash;
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * double型に変換する.
+     * double型で表した値（精度が落ちることに注意）
+     */
+    @Override
+    public double doubleValue()
+    {
+        if(denominator == null || numerator == null)
+        {
+            return Double.NaN;
+        }
+        return numerator.doubleValue() / denominator.doubleValue();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * float型に変換する.
+     * float型で表した値（精度が落ちることに注意）
+     */
+    @Override
+    public float floatValue()
+    {
+        return (float)doubleValue();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * long型に変換する.
+     * long型で表した値（精度が落ちることに注意）
+     */
+    @Override
+    public long longValue()
+    {
+        return (long)doubleValue();
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * int型に変換する.
+     * int型で表した値（精度が落ちることに注意）
+     */
+    @Override
+    public int intValue()
+    {
+        return (int)doubleValue();
+    }
+}
---
+++ test/FractionTest.java
@@ -0,0 +1,369 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.lang.reflect.Field;
+import java.math.BigInteger;
+
+import org.junit.Assert;
+import org.junit.Test;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Fractionクラスのテスト.
+ * @author 久保　由仁
+ */
+public class FractionTest
+{
+    /**
+     * 正しく分数文字列を解析できること
+     */
+    @Test
+    public void parseValidStringTest()
+    {
+        String[] validData = { "1/2", "- 1/3" };
+        for(String val : validData)
+        {
+            Object result = Fraction.parse(val);
+            Assert.assertTrue("正しく分数文字列を解析できること", result instanceof Fraction);
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * nullをparseするとnullとなること
+     */
+    @Test
+    public void parseNullTest()
+    {
+        Object result = Fraction.parse(null);
+        Assert.assertTrue("nullをparseするとnullとなること", result == null);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 不正な文字列をparseするとIllegalArgumentExceptionがスローされること
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void parseInvalidStringTest()
+    {
+        Fraction.parse("xxx");
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタでFractionオブジェクトを構築できること
+     */
+    @Test
+    public void constructorTest()
+    {
+        final String message = "コンストラクタでFractionオブジェクトを構築できること";
+        Object obj = new Fraction(BigInteger.ONE, new BigInteger("2"));
+        Assert.assertTrue(message, obj instanceof Fraction);
+        BigInteger numerator = null;
+        BigInteger denominator = null;
+        try
+        {
+            Field numeratorField = Fraction.class.getDeclaredField("numerator");
+            Field denominatorField = Fraction.class.getDeclaredField("denominator");
+            numerator = (BigInteger)numeratorField.get(obj);
+            denominator = (BigInteger)denominatorField.get(obj);
+        }
+        catch(ReflectiveOperationException e)
+        {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(denominator, new BigInteger("2"));
+        Assert.assertEquals(numerator, new BigInteger("1"));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタの引数にnullを渡すとNullPointerExceptionがスローされること
+     */
+    @Test(expected=NullPointerException.class)
+    public void constructorNullTest()
+    {
+        new Fraction(BigInteger.ONE, null);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタに分母として0を与えるとIllegalArgumentExceptionがスローされること
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void constructorWithZeroDenomialTest()
+    {
+        new Fraction(BigInteger.ONE, BigInteger.ZERO);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * コンストラクタに整数を1つ与えると分母を1とする分数が生成されること
+     */
+    @Test
+    public void constructWithIntegerTest()
+    {
+        final String message = "コンストラクタに整数を1つ与えると分母を1とする分数が生成されること";
+        Object obj = new Fraction(3);
+        Assert.assertTrue(message, obj instanceof Fraction);
+        Assert.assertEquals(message, obj, new Fraction(3,1));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の分数と正しく加算できること
+     */ 
+    @Test
+    public void addToAnotherFractionTest()
+    {
+        final String message = "他の分数と正しく加算できること";
+        Fraction frac = new Fraction(1, 3);
+        Assert.assertEquals(message, frac.add(new Fraction(1, 4)), new Fraction(7, 12));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 整数と正しく加算ができること
+     */
+    @Test
+    public void addToIntegerTest()
+    {
+        final String message = "整数と正しく加算ができること";
+        Fraction frac = new Fraction(1, 5);
+        Assert.assertEquals(message, frac.add(2), new Fraction(11, 5));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * ０を加算しても値が変わらないこと
+     */
+    @Test
+    public void addToZeroTest()
+    {
+        final String message = "０を加算しても値が変わらないこと";
+        Fraction frac = new Fraction(1, 5);
+        Assert.assertEquals(message, frac.add(0), frac);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * nullを加算しようとするとNullPointerExceptionがスローされること
+     */
+    @Test(expected=NullPointerException.class)
+    public void addNullTest()
+    {
+        Fraction frac = new Fraction(1, 5);
+        BigInteger bigNull = null;
+        frac.add(bigNull);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 正しく分数を減算できること
+     */
+    @Test
+    public void subtractAnotherFractionTest()
+    {
+        final String message = "正しく分数を減算できること";
+        Fraction frac = new Fraction(1, 3);
+        Assert.assertEquals(message, frac.subtract(new Fraction(5, 6)), new Fraction(-1,2));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 正しく整数を減算できること
+     */
+    @Test
+    public void subtractIntegerTest()
+    {
+        final String message = "正しく整数を減算できること";
+        Fraction frac = new Fraction(10, 3);
+        Assert.assertEquals(message, frac.subtract(2), new Fraction(4,3));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 同じ値を減算した結果が0となること
+     */
+    @Test
+    public void subtractSameValueTest()
+    {
+        final String message = "同じ値を減算した結果が0となること";
+        Fraction frac = new Fraction(8, 3);
+        Assert.assertEquals(message, frac.subtract(frac), new Fraction(0));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の分数と正しく乗算ができること
+     */
+    @Test
+    public void multiplyAnotherFractionTest()
+    {
+        final String message = "他の分数と正しく乗算ができること";
+        Fraction frac = new Fraction(2, 7);
+        Assert.assertEquals(message, frac.multiply(new Fraction(1, 2)), new Fraction(1, 7));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の整数と正しく乗算ができること
+     */
+    @Test
+    public void multiplyIntegerTest()
+    {
+        final String message = "他の整数と正しく乗算ができること";
+        Fraction frac = new Fraction(2, 7);
+        Assert.assertEquals(message, frac.multiply(-14), new Fraction(-4));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 0を乗じた結果が0となること
+     */
+    @Test
+    public void multiplyZeroTest()
+    {
+        final String message = "他の整数と正しく乗算ができること";
+        Fraction frac = new Fraction(2, 7);
+        Assert.assertEquals(message, frac.multiply(0), new Fraction(0));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の分数で正しく除算ができること
+     */
+    @Test
+    public void divideByAnotherFractionTest()
+    {
+        final String message = "他の分数で正しく除算ができること";
+        Fraction frac = new Fraction(14, 27);
+        Assert.assertEquals(message, frac.divide(new Fraction(2,9)), new Fraction(7, 3));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 他の整数で正しく除算ができること
+     */
+    @Test
+    public void divideByIntegerTest()
+    {
+        final String message = "他の整数で正しく除算ができること";
+        Fraction frac = new Fraction(14, 27);
+        Assert.assertEquals(message, frac.divide(2), new Fraction(7, 27));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 0で除算しようとするとIllegalArgumentExceptionがスローされること
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void divideByZeroTest()
+    {
+        Fraction frac = new Fraction(14, 27);
+        frac.divide(new Fraction(0));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 値の異なる正の分数同士で大小比較を正しく行えること
+     */
+    @Test
+    public void compareToAnotherPositiveFractionTest()
+    {
+        final String message = "値の異なる正の分数同士で大小比較を正しく行えること";
+        Fraction frac1 = new Fraction(1, 2);
+        Fraction frac2 = new Fraction(1, 3);
+        Assert.assertTrue(message, frac1.compareTo(frac2) > 0);
+        Assert.assertFalse(message, frac2.compareTo(frac1) > 0);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 値の異なる負の分数同士で大小比較を正しく行えること
+     */
+    @Test
+    public void compareToAnotherNegativeFractionTest()
+    {
+        final String message = "値の異なる負の分数同士で大小比較を正しく行えること";
+        Fraction frac1 = new Fraction(-1, 2);
+        Fraction frac2 = new Fraction(1, -3);
+        Assert.assertTrue(message, frac1.compareTo(frac2) < 0);
+        Assert.assertFalse(message, frac2.compareTo(frac1) < 0);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 約分すると同じになる分数同士で大小比較を正しく行えること
+     */
+    @Test
+    public void compareToAnotherFractionWithSameValueTest()
+    {
+        final String message = "約分すると同じになる分数同士で大小比較を正しく行えること";
+        Fraction frac = new Fraction(4, 5);
+        Assert.assertEquals(message, frac.compareTo(new Fraction(8, 10)), 0);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 分数を文字列に変換できること
+     */
+    @Test
+    public void toStringWithFractionTest()
+    {
+        final String message = "分数を文字列に変換できること";
+        Fraction frac = new Fraction(3,4);
+        Assert.assertEquals(message, frac.toString(), "(3 / 4)");
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 負の分数を文字列に変換できること
+     */
+    @Test
+    public void toStringWithNegativeFractionTest()
+    {
+        final String message = "負の分数を文字列に変換できること";
+        Fraction frac = new Fraction(-3,4);
+        Assert.assertEquals(message, frac.toString(), "- (3 / 4)");
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 約分すると整数となる分数を文字列に変換できること
+     */
+    @Test
+    public void toStringWithIntegerTest()
+    {
+        final String message = "約分すると整数となる分数を文字列に変換できること";
+        Fraction frac = new Fraction(-8, 4);
+        Assert.assertEquals(message, frac.toString(), "-2");
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * intValue()メソッドでint値を取得できること
+     */
+    @Test
+    public void intValueTest()
+    {
+        final String message = "intValue()メソッドでint値を取得できること";
+        Fraction frac = new Fraction(-8, 4);
+        Assert.assertEquals(message, frac.intValue(), -2);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * doubleValue()メソッドでdouble値を取得できること
+     */
+    @Test
+    public void doubleValueTest()
+    {
+        final String message = "doubleValue()メソッドでdouble値を取得できること";
+        Fraction frac = new Fraction(1, 4);
+        Assert.assertEquals(message, frac.doubleValue(), 0.25d, 1.0e-16);
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 同じ値の分数が同値と判定されること
+     */
+    @Test
+    public void equalsWithSameFractionTest()
+    {
+        final String message = "同じ値の分数が同値と判定されること";
+        Fraction frac = new Fraction(3, 4);
+        Assert.assertTrue(message, frac.equals(Fraction.parse("30/40")));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * 同じオブジェクトが同値と判定されること
+     */
+    @Test
+    public void equalsWithSameObjectTest()
+    {
+        final String message = "同じオブジェクトが同値と判定されること";
+        Fraction frac = new Fraction(3, 4);
+        Assert.assertTrue(message, frac.equals(frac));
+    }
+    //----------------------------------------------------------------------------------------------
+    /**
+     * nullが同値と判定されないこと
+     */
+    @Test
+    public void equalsWithNullTest()
+    {
+        final String message = "nullが同値と判定されないこと";
+        Fraction frac = new Fraction(3, 4);
+        Assert.assertFalse(message, frac.equals(null));
+    }
+}
